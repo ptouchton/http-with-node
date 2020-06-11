99b8cfc86381a0903ded1ec50f54daee888653d6
Paul Touchton <p_touchton@yahoo.com>
2020-06-10 20:58:28 -0700

    added response to the get method

--- server.js
+++ server.js
@@ -10,25 +10,32 @@ const server = https.createServer({
     cert: fs.readFileSync('./cert.pem')
 });
 server.on('request', (request, response) => {
-  const parsedUrl = url.parse(request.url, true);
-  
-  if (request.method === 'GET' && parsedUrl.pathname === '/metadata/') {
-      const { id } = parsedUrl.query;
-      const metadata = services.fetchImageMetadata(id);
-      console.log(metadata);
-  }  
-  
- jsonBody(request, response, (err, body) => {
-     if (err) {
-         console.log(err)
-     }else {
-       console.log(body);  
-       services.createUser(body['userName'])
-     }
-     console.log(body);
- })
+    const parsedUrl = url.parse(request.url, true);
 
- response.end('This was served with https!');
+    if (request.method === 'GET' && parsedUrl.pathname === '/metadata') {
+        const { id } = parsedUrl.query;
+        const metadata = services.fetchImageMetadata(id);
+        response.writeHead(200, {
+            'X-Powered-By': 'Node',
+            'Content-Type': 'application/json'
+        });
+        const serializedJson = JSON.stringify(metadata);
+        response.write(serializedJson);
+        response.end();
+    } else if (request.method === 'POST' && parsedUrl.pathname === '/users') {
+        jsonBody(request, response, (err, body) => {
+            if (err) {
+                console.log(err);
+            } else {
+                services.createUser(body['userName']);
+            }
+        });
+    } else {
+        response.writeHead(404, {
+            'X-Powered-By': 'Node'
+        });
+        response.end();
+    }
 });
 
 server.listen(443);
