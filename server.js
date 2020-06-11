f6f8fe21e51753b94ef2be309b1ca335791d8da7
Paul Touchton <p_touchton@yahoo.com>
2020-06-10 20:39:41 -0700

    added post method and setup https

--- server.js
+++ server.js
@@ -1,18 +1,34 @@
-const http = require('http');
+const https = require('https');
 const services = require('./services');
 const url = require('url');
+const jsonBody = require("body/json");
+const { request } = require('http');
+const fs = require('fs');
 
-const server = http.createServer();
+const server = https.createServer({
+    key: fs.readFileSync('./key.pem'),
+    cert: fs.readFileSync('./cert.pem')
+});
 server.on('request', (request, response) => {
   const parsedUrl = url.parse(request.url, true);
-  console.log(parsedUrl);
-  console.log(request.method);
+  
   if (request.method === 'GET' && parsedUrl.pathname === '/metadata/') {
       const { id } = parsedUrl.query;
       const metadata = services.fetchImageMetadata(id);
       console.log(metadata);
   }  
   
+ jsonBody(request, response, (err, body) => {
+     if (err) {
+         console.log(err)
+     }else {
+       console.log(body);  
+       services.createUser(body['userName'])
+     }
+     console.log(body);
+ })
+
+ response.end('This was served with https!');
 });
 
-server.listen(8080);
+server.listen(443);
